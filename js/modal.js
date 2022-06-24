385fdea0496ae5a3058bf84349aa4d17b7f72de8
Start579 <103378949+Start579@users.noreply.github.com>
2022-06-24 08:47:40 +0300

    24.06

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,4 @@
+// модальное окно
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
@@ -13,6 +14,7 @@
     refs.modal.classList.toggle("is-hidden");
   }
 })();
+// скрипт затычка для модального окна
 (() => {
   document.querySelector('.js-speaker-form').addEventListener('submit', e => {
     e.preventDefault();
@@ -22,17 +24,33 @@
     );
   });
 })();
+// мобильное меню
 (() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]");
-  const mobileMenuRef = document.querySelector("[data-menu]");
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
 
-  menuBtnRef.addEventListener("click", () => {
-    const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
 
-    menuBtnRef.classList.toggle("is-open");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
 
-    mobileMenuRef.classList.toggle("is-open");
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+
+  // Закрываем мобильное меню на более широких экранах
+  // в случае изменения ориентации устройства.
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
